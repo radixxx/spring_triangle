bbd990ab732086828ae756d12e941b3f61853f00
radixxx <radov.2011@gmail.com>
2022-01-16 13:40:54 +0200

    DI from .properties file

--- src/main/java/spring_introduction/RunTestDI.java
+++ src/main/java/spring_introduction/RunTestDI.java
@@ -7,10 +7,8 @@ public class RunTestDI {
         ClassPathXmlApplicationContext context =
             new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        Pet pet = context.getBean("myPet", Pet.class);
         Person person = context.getBean("myPerson", Person.class);
         person.callYourPet();
-
         System.out.println(person.getSurname());
         System.out.println(person.getAge());
 
