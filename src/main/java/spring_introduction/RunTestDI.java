cca3824a4328f6b83f80badebdb581d4bbca777f
radixxx <radov.2011@gmail.com>
2023-01-10 13:58:10 +0200

    fix Person

--- src/main/java/spring_introduction/RunTestDI.java
+++ src/main/java/spring_introduction/RunTestDI.java
@@ -8,7 +8,7 @@ public class RunTestDI {
             new ClassPathXmlApplicationContext("applicationContext.xml");
 
         Pet pet = context.getBean("myPet", Pet.class);
-        Person person = new Person(pet);
+        Person person = context.getBean("myPerson", Person.class);
         person.callYourPet();
 
         System.out.println(person.getSurname());
