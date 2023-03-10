b7ed85063d4ca2138f99fc5502a5b2afdef4ce5f
radixxx <radov.2011@gmail.com>
2023-03-10 12:58:01 +0200

    HQL queries delete

---
+++ src/main/java/hibernate/runner/RunnnerHibernite4.java
@@ -0,0 +1,26 @@
+package hibernate.runner;
+
+import hibernate.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class RunnnerHibernite4 {
+    public static void main(String[] args) {
+
+        SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+        try {
+            Session session = sessionFactory.getCurrentSession();
+            session.beginTransaction();
+            Employee employee = session.get(Employee.class, 2);
+            session.delete(employee);
+
+            session.getTransaction().commit();
+        } finally {
+            sessionFactory.close();
+        }
+    }
+}
